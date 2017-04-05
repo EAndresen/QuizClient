3ca6840d1d86ea29e334e1a41a3e1ac9920d5876
Eric Andresen <ericandresen91@gmail.com>
2017-04-05 13:56:19 +0200

    Version 1.0 - Commentated and cleaned up the code.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,21 +12,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Controller {
-    @FXML
-    Button connectToServerButton;
+    @FXML Button connectToServerButton;
+    @FXML TextArea outputTextArea;
+    @FXML TextField serverAddressInput;
+    @FXML TextField serverPortInput;
+    @FXML TextField inputText;
 
-    @FXML
-    TextArea outputTextArea;
-
-    @FXML
-    TextField serverAddressInput;
-
-    @FXML
-    TextField serverPortInput;
-
-    @FXML
-    TextField inputText;
-
+    //instance variables
     private String serverAddress;
     private int serverPort;
 
@@ -36,28 +28,37 @@ public class Controller {
     public Controller() {
     }
 
+    //Connecting to the server and creating a thread for incoming and
+    //outgoing data.
     public void connectToServer() {
-
         serverAddress = serverAddressInput.getText();
         serverPort = Integer.parseInt(serverPortInput.getText());
 
         new Thread(() -> {
+            //Trying to create a connection to the server and then creates a scanner
+            //for handling all incoming data.
             try {
                 Socket socket = new Socket(serverAddress, serverPort);
                 in  = socket.getInputStream();
                 out = new PrintWriter(socket.getOutputStream(), true);
                 Scanner scanner = new Scanner(in);
 
+                //Takes incoming data and print to text area.
                 while (scanner.hasNextLine()) {
                     outputTextArea.appendText(scanner.nextLine() + "\n");
                 }
                 socket.close();
+
+            //If connection were not possible do make, tell the user.
             } catch (IOException e) {
                 System.out.println(e);
                 outputTextArea.appendText("No server connection: \n" + e + "\n");
             }
         }).start();
     }
+
+    //Listening for Enter press and send button pressed.
+    //Take input data from text field and send it to the server.
     public void onEnter(){
         out.println(inputText.getText());
         inputText.setText("");
